3456b66291dbfe33093629fe9303693c20dd7e39
caiocgomes <caio@caiogomes.com.br>
2025-05-11 18:55:30 -0300

    v11 - fixing critical

--- scripts/crit-damage.js
+++ scripts/crit-damage.js
@@ -14,7 +14,7 @@ Hooks.once("ready", () => {
       const damageRoll = await wrapped(...args);
   
       // Só modifica se for dano crítico
-      if (!config.options.critical) {
+      if (!config?.critical) {
         console.log("config: ",config)
         console.log("Dano normal: ",damageRoll)
         return damageRoll;
